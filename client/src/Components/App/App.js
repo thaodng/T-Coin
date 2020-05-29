cd079bd79554c26afd12f8294a0e080f84c6a240
Duy Nguyen <ngduythao2805@outlook.com>
2020-05-29 21:13:53 +0700

    Front-end structure

--- client/src/Components/App/App.js
+++ client/src/Components/App/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() {  
+  console.log(process.env);
   return (
     <div className="App">
       <header className="App-header">
